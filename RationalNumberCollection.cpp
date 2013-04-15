e94956d948050a2abb3e0e4be300451af95c6aff
admin <administrator@ubuntu.(none)>
2013-04-15 16:43:00 +0200

    new classes

---
+++ RationalNumberCollection.cpp
@@ -0,0 +1,3 @@
+#include "RationalNumberCollection.h"
+
+
---
+++ RationalNumberCollection.h
@@ -0,0 +1,12 @@
+#ifndef RATIONALNUMBERCOLLECTION_H
+#define RATIONALNUMBERCOLLECTION_H
+
+struct RationalNumberCollection{
+
+    Array liste;
+
+};
+
+
+
+#endif // RATIONALNUMBERCOLLECTION_H
