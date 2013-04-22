ed35307059e9cf374a1c8f00d13f48e8a0357928
Marco <m_seidler@hotmail.de>
2013-04-22 13:17:40 +0200

    binaere Suche hinzugefuegt

---
+++ binary_search.h
@@ -0,0 +1,8 @@
+#ifndef BINARY_SEARCH_H
+#define BINARY_SEARCH_H
+
+#include "rationalnumbercollection.h"
+
+int bin_search(RationalNumberCollection *c, RationalNumber n);
+
+#endif // BINARY_SEARCH_H
